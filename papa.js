29cb20284f84f385e9118cd60a476b3b74a4afe1
roman <mediavisionport@gmail.com>
2019-11-29 18:00:09 +0200

    firstComit

---
+++ papa.js
@@ -0,0 +1,46 @@
+ 'use strict';
+// console.log(leftBorder);
+// var leftBorder = 1;
+// let second = 2;
+
+// console.log(second);
+// console.log(leftBorder);
+// console.log(leftBorder);
+
+// let person = {
+//     age: 45 ,
+//     name: "piter",
+//     jenat: true
+// }
+
+
+// console.log(person);
+// console.log(person.age);
+// console.log (person["name"]);
+
+// let arr = ['red','blu','green'];
+// console.log(arr[2]);
+// console.log( arr ,arr[2] );
+
+// alert('hai');
+// confirm('ti roma?');
+
+// // let answer = confirm('ti tocno roma?');
+// // console.log(answer);
+
+// let answer = +prompt('tebe  est 18?',"da");
+// console.log(typeof(answer));
+
+let incr =10,
+    decr = 10;
+incr++;
+decr--;
+    console.log(++incr,--decr);
+    console.log(5%2);
+
+    console.log("2"== 2);
+
+    let isChecked =true,
+        isClose =false;
+        console.log(isChecked && isClose);
+
